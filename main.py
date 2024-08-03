6ea47269b76d0cc65c535bd32a89045116b79719
rulkimi <azrulkimi00@gmail.com>
2024-08-03 22:31:20 +0800

    search by company

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
+import yfinance as yf
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
 import pandas as pd
 
 app = FastAPI()
+
+def get_share_price(company: str):
+  data = yf.download(company, start="2020-01-01", end="2021-01-01")
+  # Save the DataFrame to an Excel file
+  data.to_excel('aapl_data.xlsx', engine='openpyxl')
 
 def read_excel_to_json(file_path: str):
     data = pd.read_excel(file_path, engine='openpyxl')
@@ -11,9 +17,9 @@ def read_excel_to_json(file_path: str):
     return data.to_dict(orient='records')
 
 @app.get("/data")
-async def get_data():
-    file_path = 'aapl_data.xlsx'  # Path to your Excel file
-    json_data = read_excel_to_json(file_path)
+async def get_data(company: str = "AAPL"):
+    file_path = get_share_price(company)  # Path to your Excel file
+    json_data = read_excel_to_json("aapl_data.xlsx")
     return JSONResponse(content=json_data)
 
 if __name__ == "__main__":
