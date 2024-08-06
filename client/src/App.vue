a8fce05a3ecc7f4190c2d85f3795c75d101a7342
rulkimi <azrulkimi00@gmail.com>
2024-08-06 08:12:17 +0800

    populate real data

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,28 +1,37 @@
 <script setup>
-import { onMounted } from 'vue';
+import { ref, onMounted } from 'vue';
 import StockChart from './components/StockChart.vue';
 import axios from 'axios';
 
+const msftData = ref([]);
+const appleData = ref([]);
+
 onMounted(() => {
-  getSharePrice();
+  getSharePrice('AAPL');
+  getSharePrice('MSFT');
 });
 
-const getSharePrice = async () => {
+const getSharePrice = async (tickerCode) => {
   try {
-    const response = await axios.get(`http://localhost:8000/share_price/AAPL`, {
+    const response = await axios.get(`http://localhost:8000/share_price/${tickerCode}`, {
       params: {
-        start_date: '2020-05-02',
-        end_date: '2020-05-10'
+        start_date: '2020-01-01',
+        end_date: '2020-09-02'
       }
     });
     const { data } = response;
-    console.log(data)
+    
+    if (tickerCode === 'AAPL') {
+      appleData.value = data;
+    } else if (tickerCode === 'MSFT') {
+      msftData.value = data;
+    }
   } catch (error) {
-
+    console.error(`Error fetching share price for ${tickerCode}:`, error);
   }
-}
+};
 </script>
 
 <template>
-  <StockChart />
+  <StockChart :msft-data="msftData" :apple-data="appleData" />
 </template>
--- client/src/components/StockChart.vue
+++ client/src/components/StockChart.vue
@@ -1,87 +1,107 @@
-<!-- App.vue -->
 <template>
   <CanvasJSStockChart :options="options" :style="styleOptions" />
 </template>
 
-<script>
-import msftData from "../assets/msft2020.json";
-import dummyData from '../assets/randomised.json';
+<script setup>
+import { computed } from 'vue';
+import { toRefs } from 'vue';
+import { defineProps } from 'vue';
 
-export default {
-  data() {
-    const msftDps = [];
-    const dummyDps = [];
-    const msftDps2 = [];
-    const dummyDps2 = [];
-
-    msftData.forEach(data => {
-      msftDps.push({ x: new Date(data["date"]), y: [data["open"], data["high"], data["low"], data["close"]] });
-      msftDps2.push({ x: new Date(data["date"]), y: data["close"] });
-    });
-
-    dummyData.forEach(data => {
-      dummyDps.push({ x: new Date(data["date"]), y: [data["open"], data["high"], data["low"], data["close"]] });
-      dummyDps2.push({ x: new Date(data["date"]), y: data["close"] });
-    });
-
-    return {
-      chart: null,
-      options: {
-        exportEnabled: true,
-        theme: "light2",
-        title: {
-          text: "Vue.js StockChart with Date-Time Axis"
-        },
-        subtitles: [{
-          text: "MSFT Stock Price"
-        }],
-        charts: [{
-          axisY: {
-            title: "Price",
-            prefix: "$",
-            tickLength: 0
-          },
-          data: [
-            {
-              type: "candlestick",
-              name: "MSFT Price (in USD)",
-              yValueFormatString: "$#,###.##",
-              dataPoints: msftDps
-            },
-            {
-              type: "candlestick",
-              name: "Dummy Data Price (in USD)",
-              yValueFormatString: "$#,###.##",
-              dataPoints: dummyDps
-            }
-          ]
-        }],
-        navigator: {
-          data: [
-            {
-              type: "line",
-              name: "MSFT",
-              color: "blue",
-              dataPoints: msftDps2
-            },
-            {
-              type: "line",
-              name: "Dummy Data",
-              color: "red",
-              dataPoints: dummyDps2
-            }
-          ],
-          slider: {
-            minimum: new Date(2020, 0, 1),
-            maximum: new Date(2020, 11, 1)
-          }
-        }
-      },
-      styleOptions: {
-        width: "100%",
-        height: "460px"
-      }
-    };
+const props = defineProps({
+  msftData: {
+    type: Array,
+    required: true
+  },
+  appleData: {
+    type: Array,
+    required: true
   }
-}
+});
+
+const { msftData, appleData } = toRefs(props);
+
+const msftDps = computed(() => 
+  msftData.value.map(data => ({
+    x: new Date(data["Date"]),
+    y: [data["Open"], data["High"], data["Low"], data["Close"]]
+  }))
+);
+
+const msftDps2 = computed(() => 
+  msftData.value.map(data => ({
+    x: new Date(data["Date"]),
+    y: data["Close"]
+  }))
+);
+
+const dummyDps = computed(() => 
+  appleData.value.map(data => ({
+    x: new Date(data["Date"]),
+    y: [data["Open"], data["High"], data["Low"], data["Close"]]
+  }))
+);
+
+const dummyDps2 = computed(() => 
+  appleData.value.map(data => ({
+    x: new Date(data["Date"]),
+    y: data["Close"]
+  }))
+);
+
+const options = computed(() => ({
+  exportEnabled: true,
+  theme: "light2",
+  title: {
+    text: "Vue.js StockChart with Date-Time Axis"
+  },
+  subtitles: [{
+    text: "MSFT Stock Price"
+  }],
+  charts: [{
+    axisY: {
+      title: "Price",
+      prefix: "$",
+      tickLength: 0
+    },
+    data: [
+      {
+        type: "candlestick",
+        name: "MSFT Price (in USD)",
+        yValueFormatString: "$#,###.##",
+        dataPoints: msftDps.value
+      },
+      {
+        type: "candlestick",
+        name: "Dummy Data Price (in USD)",
+        yValueFormatString: "$#,###.##",
+        dataPoints: dummyDps.value
+      }
+    ]
+  }],
+  navigator: {
+    data: [
+      {
+        type: "line",
+        name: "MSFT",
+        color: "blue",
+        dataPoints: msftDps2.value
+      },
+      {
+        type: "line",
+        name: "Dummy Data",
+        color: "red",
+        dataPoints: dummyDps2.value
+      }
+    ],
+    slider: {
+      minimum: new Date(2020, 0, 1),
+      maximum: new Date(2020, 11, 1)
+    }
+  }
+}));
+
+const styleOptions = {
+  width: "100%",
+  height: "460px"
+};
 </script>
